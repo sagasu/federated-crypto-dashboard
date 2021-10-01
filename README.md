16ad9d51d51bba9dbe936329139ea3961b9760da
Mateusz Kopij <sagasu@gmail.com>
2021-10-01 20:04:10 +0100

    creating basic ui to display data.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This is a schema first approach, schema to create db is in [schema.graphql file]
 React project is in [crypto-dashboard](./crypto-dashboard) folder.  
 
 Run `npm run start-deals` to serve deals, before you run `npm run gateway` because federation is going to point to port 4001 that is being opened by `start-deals`.   
+
+To run FE run (make sure that `start-deals` and `gateway` are already running):
+`cd crypto-dashboard`  
+`npm start`  
+
--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,11 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+const  App = () => {
+  const [deals, setDeals] = useState(null);
+
+  const fetchData = async () => {
+    const url = 'http://localhost:4001';
+    const query = `
+      query {
+        deals{
+          id
+          volume
+          timestamp
+          coin {
+            asset_id
+            category
+            name
+            price_usd
+          }
+        }
+      }
+    `;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json',
+        'x-cassandra-token': process.env.REACT_APP_ASTRA_TOKEN
+      },
+      body: JSON.stringify({query})
+    });
+
+    const responseBody = await response.json();
+
+    setDeals(responseBody.data?.deals);
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+  const formatDate = (str) => {
+    return new Date(str).toDateString();
+  };
+
+  const formatPrice = (price) => {
+    return price.toFixed(2);
+  };
+
+  const assignImage =(asset_id) => {
+    return `../images/${asset_id}.png`
+  }
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      {deals && 
+        <div className="app">
+          <h1></h1>
+          <div className="overview">
+            <table>
+              <thead>
+                <tr className="header">
+                  <th></th>
+                  <th>Asset Id</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Volume</th>
+                  <th>Gain/Lose</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              {deals.map((deal, index) => (
+                <tbody key={index}>
+                  <tr>
+                    <td>
+                      <img alt='asset' src={assignImage(deal.cooin.asset_id)}/>
+                    </td>
+                    <td>{deal.coin.asset_id}</td>
+                    <td>{deal.coin.name}</td>
+                    <td>{`$${formatPrice(deal.coin.price_usd)}`}</td>
+                    <td>{deal.volume}</td>
+                    <td>{`$${formatPrice(deal.volume * deal.coin.price_usd)}`}</td>
+                    <td>{formatDate(deal.timestamp)}</td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+          </div>
+        </div>
+      }
+    </>
   );
 }
 
